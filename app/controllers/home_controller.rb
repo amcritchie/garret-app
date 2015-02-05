e7d87c1e2cef06d296814b6425e6b80441326950
Alex McRitchie <amcritchie@gmail.com>
2015-02-04 18:29:49 -0700

    add admin and shell of adding a restaurant

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @user = User.new
-
+    @all_users = User.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128042513) do
+ActiveRecord::Schema.define(version: 20150205004310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150128042513) do
     t.string   "zip"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin"
   end
 
 end
