f70ab8b117135b7ec8eb80067ec005ca8ab3ae44
Alex McRitchie <amcritchie@gmail.com>
2015-06-07 18:05:36 -0600

    evaluation questions score is saved with iron notation

--- app/assets/javascripts/evaluation/evaluationScore.js
+++ app/assets/javascripts/evaluation/evaluationScore.js
@@ -1,18 +1,18 @@
 var EvaluationScore = {
     load: function (score) {
-        score.split('|').forEach(function (question) {
+        score.split('|Θ').forEach(function (question) {
             if (question) {
-                var id = question.split(':')[0];
-                if (question.split(':')[1] === '1') {
+                var id = question.split('Æχ')[0];
+                if (question.split('Æχ')[1] === '1') {
                     $('.questionPass[data-question-id=' + id + ']').click();
                     $('.questionNil[data-question-id=' + id + ']').hide();
-                } else if (question.split(':')[1] === '0') {
+                } else if (question.split('Æχ')[1] === '0') {
                     $('.questionFail[data-question-id=' + id + ']').click();
-                    $('.answerExplanation[data-question-id=' + id + ']').show().val(question.split(':')[2]);
+                    $('.answerExplanation[data-question-id=' + id + ']').show().val(question.split('Æχ')[2]);
                     $('.questionNil[data-question-id=' + id + ']').hide();
-                } else if (question.split(':')[1] === '2') {
+                } else if (question.split('Æχ')[1] === '2') {
                     $('.questionNA[data-question-id=' + id + ']').click();
-                    $('.answerExplanation[data-question-id=' + id + ']').show().val(question.split(':')[2]);
+                    $('.answerExplanation[data-question-id=' + id + ']').show().val(question.split('Æχ')[2]);
                     $('.questionNil[data-question-id=' + id + ']').hide();
                 }
 
@@ -27,13 +27,13 @@ var EvaluationScore = {
         $('.questionCheckboxAll[data-relevant=true]').each(function (index, question) {
             var id = $(question).data('question-id');
             if ($($(this).children()[1]).hasClass('active')) {
-                array.push([id + ':' + 0 + ':' + $(this).parent().find('.answerExplanation').val()]);
+                array.push([id + 'Æχ' + 0 + 'Æχ' + $(this).parent().find('.answerExplanation').val()]);
             } else if ($($(this).children()[0]).hasClass('active')) {
-                array.push([id + ':' + 2 + ':' + $(this).parent().find('.answerExplanation').val()]);
+                array.push([id + 'Æχ' + 2 + 'Æχ' + $(this).parent().find('.answerExplanation').val()]);
             } else if ($($(this).children()[3]).hasClass('active')) {
-                array.push([id + ':' + 1]);
+                array.push([id + 'Æχ' + 1]);
             }
         });
-        return array.join('|')
+        return array.join('|Θ')
     }
 };
