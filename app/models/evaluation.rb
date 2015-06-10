1fd6dacceb0d542241e3f41712d8fd0ad2108bce
Alex McRitchie <amcritchie@gmail.com>
2015-06-10 16:46:35 -0600

    Adding favicon and page title.

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,12 +2,4 @@ class Evaluation < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :standard
   has_many :evaluation_applications
-
-  # has_secure_password
-  #
-  # validates_presence_of :name
-  # validates_uniqueness_of :email
-  # validates :password_digest, length: {minimum: 8}
-  # validates :username, length: {maximum: 10, minimum: 4}
-
 end
