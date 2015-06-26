db1abe6258cb405fdc9c44005a1aa8bba1de1427
Alex McRitchie <amcritchie@gmail.com>
2015-06-25 22:41:54 -0600

    page breaks on summery page.

--- app/assets/javascripts/action_plan/departmentSummary.js
+++ app/assets/javascripts/action_plan/departmentSummary.js
@@ -4,7 +4,7 @@ var DepartmentSummary = {
         debugger;
     },
     createHTML: function (department, evaluation) {
-        var tableHTML = '<div class="border-green align-center"><h1>' + department.departmentName + '</h1><table class="table table-bordered align-center"><thead><tr><th class="col-xs-7">Question</th><th class="col-xs-2">Fulfillment</th><th class="col-xs-3">Comment</th></tr></thead><tbody>';
+        var tableHTML = '<div class="page-break border-green align-center"><h1>' + department.departmentName + '</h1><table class="table table-bordered align-center"><thead><tr><th class="col-xs-7">Question</th><th class="col-xs-2">Fulfillment</th><th class="col-xs-3">Comment</th></tr></thead><tbody>';
         var departmentQuestions = $.grep(evaluation.scores, function (e) {
             return e.dep == department.departmentName;
         });
